fd54c5836bf4157a89b8f087086929f2c789a9c0
Ajith Ramachandran <ajithramachandran594@gmail.com>
2026-07-26 12:37:23 -0600

    3.4

--- exercises/06.object-methods/01.problem.keys-values/index.ts
+++ exercises/06.object-methods/01.problem.keys-values/index.ts
@@ -2,21 +2,27 @@
 // Turning object data into arrays
 
 const inventory = {
-	apples: 12,
-	oranges: 8,
-	bananas: 0,
-	mangoes: 4,
+  apples: 12,
+  oranges: 8,
+  bananas: 0,
+  mangoes: 4,
 }
 
-// 🐨 Use Object.keys to get an array of item names
+// Use Object.keys to get an array of item names
+const itemNames = Object.keys(inventory)
 
-// 🐨 Use Object.values to get an array of quantities
+// Use Object.values to get an array of quantities
+const quantities = Object.values(inventory)
 
-// 🐨 Use the quantities array to compute the total quantity
+// Use the quantities array to compute the total quantity
+const totalQuantity = quantities.reduce(
+  (total, quantity) => total + quantity,
+  0
+)
 
 // console.log('Item names:', itemNames)
 // console.log('Quantities:', quantities)
 // console.log('Total:', totalQuantity)
 
-// 🐨 Export your variables so we can verify your work
-// export { itemNames, quantities, totalQuantity }
+// Export your variables so we can verify your work
+export { itemNames, quantities, totalQuantity }
--- exercises/06.object-methods/02.problem.entries-from-entries/index.ts
+++ exercises/06.object-methods/02.problem.entries-from-entries/index.ts
@@ -2,19 +2,25 @@
 // Transforming object values with array methods
 
 const priceBySku = {
-	'sku-1': 1299,
-	'sku-2': 499,
-	'sku-3': 2500,
+  'sku-1': 1299,
+  'sku-2': 499,
+  'sku-3': 2500,
 }
 
-// 🐨 Use Object.entries to get an array of [sku, price] pairs
+// Use Object.entries to get an array of [sku, price] pairs
+const entries = Object.entries(priceBySku)
 
-// 🐨 Create discounted entries with 10% off (rounded to the nearest whole number)
+// Create discounted entries with 10% off (rounded to nearest whole number)
+const discountedEntries = entries.map(([sku, price]) => [
+  sku,
+  Math.round(price * 0.9),
+])
 
-// 🐨 Use Object.fromEntries to create a discountedPrices object
+// Use Object.fromEntries to create a discountedPrices object
+const discountedPrices = Object.fromEntries(discountedEntries)
 
 // console.log('Entries:', entries)
 // console.log('Discounted:', discountedPrices)
 
-// 🐨 Export your variables so we can verify your work
-// export { entries, discountedEntries, discountedPrices }
+// Export your variables so we can verify your work
+export { entries, discountedEntries, discountedPrices }
