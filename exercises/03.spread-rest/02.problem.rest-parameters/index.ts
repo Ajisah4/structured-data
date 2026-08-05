e38c067f2c394a453e87306e8f5abe637d587bb3
Ajith Ramachandran <ajithramachandran594@gmail.com>
2026-08-05 16:38:29 -0600

    2.7

--- exercises/03.spread-rest/02.problem.rest-parameters/index.ts
+++ exercises/03.spread-rest/02.problem.rest-parameters/index.ts
@@ -1,38 +1,45 @@
 // Rest Parameters
 // Functions that accept variable numbers of arguments
 
-// Create multiply
-const multiply = (...numbers: number[]): number => {
-	return numbers.reduce((product, number) => product * number, 1)
+// Rest parameters collect all arguments into an array
+function multiply(...numbers: Array<number>) {
+	let product = 1
+	for (const n of numbers) {
+		product *= n
+	}
+	return product
 }
 
-// Create logWithPrefix
-const logWithPrefix = (prefix: string, ...messages: string[]): void => {
-	messages.forEach((message) => {
+// Combining regular parameter with rest
+function logWithPrefix(prefix: string, ...messages: Array<string>) {
+	for (const message of messages) {
 		console.log(`${prefix} ${message}`)
-	})
+	}
 }
 
-// Create sum
-const sum = (...numbers: number[]): number => {
-	return numbers.reduce((total, number) => total + number, 0)
+// Sum all numbers using rest parameters
+function sum(...numbers: Array<number>) {
+	let total = 0
+	for (const n of numbers) {
+		total += n
+	}
+	return total
 }
 
-// Create mergeArrays
-const mergeArrays = (...arrays: number[][]): number[] => {
-	return arrays.reduce((merged, array) => [...merged, ...array], [])
+// Rest + spread combo - collect arrays, spread to merge
+function mergeArrays(...arrays: Array<Array<number>>) {
+	const result: Array<number> = []
+	for (const arr of arrays) {
+		result.push(...arr)
+	}
+	return result
 }
 
-// Test
 console.log(multiply(2, 3, 4)) // 24
 console.log(multiply(5)) // 5
 console.log(multiply()) // 1
-
 logWithPrefix('[INFO]', 'Starting', 'Processing', 'Done')
-
 console.log(sum(1, 2, 3, 4)) // 10
-
 console.log(mergeArrays([1, 2], [3, 4], [5])) // [1, 2, 3, 4, 5]
 
-// Export
-export { multiply, sum, mergeArrays }2.7
+export { multiply, sum, mergeArrays }
